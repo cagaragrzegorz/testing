e841abc4767581c46248ea08fbd44dbf8d740d21
Cagara, Grzegorz <grzegorz.cagara@sabre.com>
2019-01-08 00:05:51 +0100

    introducing initial working logic

--- java-junit/src/main/java/JavaJUnitMain.java
+++ java-junit/src/main/java/JavaJUnitMain.java
@@ -1,6 +1,13 @@
+import domain.Game;
+import domain.GameFactory;
+
 public class JavaJUnitMain {
 
+    private static final GameFactory gameFactory = new GameFactory();
+
     public static void main(String[] args) {
-        System.out.println("grzegorz");
+        Game game = gameFactory.createGame();
+
+        System.out.println(game.toString());
     }
 }
--- java-junit/src/main/java/domain/Game.java
+++ java-junit/src/main/java/domain/Game.java
@@ -9,4 +9,11 @@ public class Game {
     public Double rank;
     public GameType type;
     public boolean isInPossession;
+
+
+    @Override
+    public String toString() {
+        return String.format("Game Name: %s, Price: %.2f, Rank: %.1f, Type: %s, Do I have it?: %s",
+                name, price, rank, type, isInPossession);
+    }
 }
