eac45ec5f261edf630debce582cf3867a96d58a6
Cagara, Grzegorz <grzegorz.cagara@sabre.com>
2019-01-28 23:07:21 +0100

    adding JUnit tests

---
+++ java-junit/src/test/java/com/testing/runners/AppRunnerTest.java
@@ -0,0 +1,14 @@
+package com.testing.runners;
+
+import org.junit.Test;
+
+public class AppRunnerTest {
+
+    @Test
+    public void filterMostRankedGame() {
+    }
+
+    @Test
+    public void filterBestGame() {
+    }
+}
